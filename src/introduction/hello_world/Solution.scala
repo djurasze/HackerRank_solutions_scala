9dc0cc0c2c74bc6776dccdff7ffd7243f3da2f2b
djurasze <djurasze@gmail.com>
2020-04-15 22:14:50 +0200

    Hello world

--- src/introduction/hello_world/Solution.scala
+++ src/introduction/hello_world/Solution.scala
@@ -4,8 +4,16 @@ object Solution extends App {
 
   def f(): Unit = println("Hello World")
 
+//  def f(n: Int) {
+//    for(i <- 1 to n) {
+//      println("Hello World")
+//    }
+//  }
+
+  def f(n: Int): Unit = LazyList.fill(n)("Hello World") foreach {println}
 
   f()
+  f(10)
 }
 
 
