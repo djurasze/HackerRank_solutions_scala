ff489c8a94fe45f051d3c03238b5ca804ceb8d4a
djurasze <djurasze@gmail.com>
2020-04-20 21:28:58 +0200

    Add solution to filter array problem

---
+++ src/introduction/filter_array/Solution.scala
@@ -0,0 +1,19 @@
+package introduction.filter_array
+
+import scala.io.Source
+
+object Solution {
+
+  def f(delim:Int,arr:List[Int]):List[Int] = arr.filter(_ < delim)
+
+  def main(args: Array[String]): Unit = {
+    val source = Source.fromFile("D:\\src\\HackerRank_solutions_scala\\src\\introduction\\filter_array\\data.txt")
+
+    println(f(source.getLines().next().toInt, source.getLines().map(_.toInt).toList))
+
+    source.close()
+  }
+
+}
+
+
